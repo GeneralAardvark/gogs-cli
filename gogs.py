52adc3f4f4d461a092ea5f4bbcd49ea204842050
Simon Whittle <simon@bashton.com>
2016-09-13 15:33:57 +0100

    Initial commit

---
+++ gogs.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import requests
+import argparse
+from configparser import ConfigParser
+import sys
+import os
+import re
+
+cfg = ConfigParser()
+cfg_file = os.path.join(os.path.expanduser("~"), '.config', 'gogs.cfg')
+
+s = requests.Session()
+
+def readConfig():
+    cfg = ConfigParser()
+    cfg_file = os.path.join(os.path.expanduser("~"), '.config', 'gogs.cfg')
+    if cfg.read(cfg_file):
+        c = []
+        for e in ('gogs_host', 'token', 'username'):
+            try:
+                c.append(cfg['gogs'][e])
+            except:
+                sys.exit("'{}' missing or incorrectly formatted in {}.".format(e, cfg_file))
+        return c
+    else:
+        sys.exit("{} not found or incorrectly formatted.".format(cfg_file))
+
+
+def validateConfig(c):
+    global api
+    global username
+    gogs_host, token, username = c
+    api = "https://{}/api/v1".format(gogs_host)
+    api += "/{}"
+    s.headers = {'Authorization':'token {}'.format(token)}
+    try:
+        r = callAPI('users/{}'.format(username), True)
+    except:
+        sys.exit("Error connecting to {}.".format(gogs_host))
+    if r.status_code == 404:
+        sys.exit("Does username {}, exist?".format(username))
+    if r.status_code == 403:
+        sys.exit("Unable to authenticate with token specified.")
+    if r.status_code != 200:
+        sys.exit("Got a {} when connecting to {}.".format(r.status_code, gogs_user))
+
+
+def callAPI(command, test=False):
+    r = s.get(api.format(command))
+    if test:
+        return r
+    if r.status_code == 200:
+        return r.json()
+    # Code to get around 500 error when getting branches for empty repos.
+    if r.status_code == 500 and "branches" in command:
+        return []
+    # End.
+    sys.exit("{} when calling {}.".format(r.status_code, command))
+
+
+class cOut:
+    red = '\033[1;31m'
+    green = '\033[1;32m'
+    yellow = '\033[1;33m'
+    end = '\033[1;m'
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Dirty Gogs Repo Searcher")
+
+    options = parser.add_mutually_exclusive_group()
+    options.add_argument('-clone', help="git clone commands.", action="store_true")
+
+    output_opts = options.add_argument_group()
+    output_opts.add_argument('-url', help="Show URL link", action="store_true")
+    output_opts.add_argument('-ssh', help="Show git ssh url", action="store_true")
+
+    branches = options.add_mutually_exclusive_group()
+    branches.add_argument('-branch', help="Show branches.", action="store_true")
+    branches.add_argument('-mybranch', help="Show my branches.", action="store_true")
+
+    parser.add_argument('search', nargs="?", help="Search term for repo.", default=".")
+
+    args = parser.parse_args()
+
+    validateConfig(readConfig())
+
+    repos = callAPI('user/repos')
+
+    if len(repos) == 0:
+        sys.exit('Error, no repos to list')
+
+    searchterm = re.compile(args.search, re.I)
+
+    for repo in repos:
+        if searchterm.search(repo['full_name']):
+            if args.clone:
+                print("git clone {}".format(repo['ssh_url']))
+                continue
+            output = repo['full_name']
+            if args.url:
+                output += " {}{}{}".format(cOut.red, repo['html_url'], cOut.end)
+            if args.ssh:
+                output += " {}{}{}".format(cOut.green, repo['ssh_url'], cOut.end)
+            if args.branch or args.mybranch:
+                branches = callAPI('repos/{}/branches'.format(repo['full_name']))
+                for branch in branches:
+                    if args.mybranch:
+                        if branch['commit']['author']['username'] != username:
+                            continue
+                    print(output)
+                    print("\t{}{} {}{}{}".format(
+                        cOut.green,
+                        branch['commit']['author']['username'],
+                        cOut.yellow,
+                        branch['name'],
+                        cOut.end))
+            else:
+                print(output)
+
+
+if __name__ == '__main__':
+    main()
