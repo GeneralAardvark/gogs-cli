f655a859667261bb3f0fbae392d13e9307a2a034
Simon Whittle <simon@bashton.com>
2016-09-22 09:12:26 +0100

    some cleanup

--- gogs.py
+++ gogs.py
@@ -12,10 +12,10 @@ import re
 
 cfg = ConfigParser()
 cfg_file = os.path.join(os.path.expanduser("~"), '.config', 'gogs.cfg')
-
 s = requests.Session()
 
-def readConfig():
+
+def read_config():
     cfg = ConfigParser()
     cfg_file = os.path.join(os.path.expanduser("~"), '.config', 'gogs.cfg')
     if cfg.read(cfg_file):
@@ -30,15 +30,15 @@ def readConfig():
         sys.exit("{} not found or incorrectly formatted.".format(cfg_file))
 
 
-def validateConfig(c):
+def validate_config(c):
     global api
     global username
     gogs_host, token, username = c
     api = "https://{}/api/v1".format(gogs_host)
     api += "/{}"
-    s.headers = {'Authorization':'token {}'.format(token)}
+    s.headers = {'Authorization': 'token {}'.format(token)}
     try:
-        r = callAPI('user/', True)
+        r = call_api('user/', True)
     except:
         sys.exit("Error connecting to {}.".format(gogs_host))
     if r.status_code == 404:
@@ -50,7 +50,8 @@ def validateConfig(c):
     if r.json()['username'] != username:
         sys.exit("Token and username, {}, do not match.".format(username))
 
-def callAPI(command, test=False):
+
+def call_api(command, test=False):
     r = s.get(api.format(command))
     if test:
         return r
@@ -63,7 +64,7 @@ def callAPI(command, test=False):
     sys.exit("{} when calling {}.".format(r.status_code, command))
 
 
-class cOut:
+class colour:
     red = '\033[1;31m'
     green = '\033[1;32m'
     yellow = '\033[1;33m'
@@ -72,24 +73,19 @@ class cOut:
 
 def main():
     parser = argparse.ArgumentParser(description="Dirty Gogs Repo Searcher")
-
     options = parser.add_mutually_exclusive_group()
     options.add_argument('-clone', help="git clone commands.", action="store_true")
-
     output_opts = options.add_argument_group()
     output_opts.add_argument('-ssh', help="Show git ssh url", action="store_true")
-
     branches = options.add_mutually_exclusive_group()
     branches.add_argument('-branches', help="Show branches.", action="store_true")
     branches.add_argument('-mybranches', help="Show my branches.", action="store_true")
-
     parser.add_argument('search', nargs="?", help="Search term for repo.", default=".")
-
     args = parser.parse_args()
 
-    validateConfig(readConfig())
+    validate_config(read_config())
 
-    repos = callAPI('user/repos')
+    repos = call_api('user/repos')
 
     if len(repos) == 0:
         sys.exit('Error, no repos to list')
@@ -102,22 +98,22 @@ def main():
                 print("git clone {}".format(repo['ssh_url']))
                 continue
             output = repo['full_name']
-            output += " {}{}{}".format(cOut.red, repo['html_url'], cOut.end)
+            output += " {}{}{}".format(colour.red, repo['html_url'], colour.end)
             if args.ssh:
-                output += " {}{}{}".format(cOut.green, repo['ssh_url'], cOut.end)
+                output += " {}{}{}".format(colour.green, repo['ssh_url'], colour.end)
             if args.branches or args.mybranches:
-                branches = callAPI('repos/{}/branches'.format(repo['full_name']))
+                branches = call_api('repos/{}/branches'.format(repo['full_name']))
                 for branch in branches:
                     if args.mybranches:
                         if branch['commit']['author']['username'] != username:
                             continue
                     print(output)
                     print("\t{}{} {}{}{}".format(
-                        cOut.green,
+                        colour.green,
                         branch['commit']['author']['username'],
-                        cOut.yellow,
+                        colour.yellow,
                         branch['name'],
-                        cOut.end))
+                        colour.end))
             else:
                 print(output)
 
